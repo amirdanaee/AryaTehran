9e1d54b65a2d15e909d0743f4433105ddd383eff
amirdanaee <danaeeebrahimi@gmail.com>
2025-07-18 20:14:14 +0330

    Getting input data from the user

--- RocketLauncher.py
+++ RocketLauncher.py
@@ -3,12 +3,24 @@ import turtle as t
 t.showturtle()
 t.speed(0)
 t.penup()
+
+# Circle1
 t.goto(200,0)
 t.dot(50)
+# Circle2
 t.goto(300,200)
 t.dot(50)
+# Circle3
 t.goto(300,-200)
 t.dot(50)
+
 t.goto(-200,0)
+t.pendown()
+t.pencolor("red")
+# Getting input data from the user
+rotation = int(input("Enter the degree of rotation..."))
+distance = int(input("Enter the distance...")) 
+t.left(rotation)
+t.forward(distance)
 
 t.done()
