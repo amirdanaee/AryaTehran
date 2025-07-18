5fe2a030fe56c832ad4672b5062db3967e457e68
amirdanaee <danaeeebrahimi@gmail.com>
2025-07-18 19:38:46 +0330

    Making a game board

---
+++ RocketLauncher.py
@@ -0,0 +1,14 @@
+import turtle as t
+# Making a game board
+t.showturtle()
+t.speed(0)
+t.penup()
+t.goto(200,0)
+t.dot(50)
+t.goto(300,200)
+t.dot(50)
+t.goto(300,-200)
+t.dot(50)
+t.goto(-200,0)
+
+t.done()
